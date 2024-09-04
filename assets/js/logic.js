72c24785f635b7758e985978f711a94c47c105fd
MauroIturra <mauroiturra1989@gmail.com>
2024-09-04 03:55:01 -0400

    modificando desafio

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,21 +11,26 @@ fetch("https://www.superheroapi.com/api.php/5097b85a9557dc7a49614f9d5575ca28/"+i
 .then(r=>r.json())
 .then(d => {
 
+console.log(d);   
+
 Heroe.setAttribute("src", d.image.url);
+document.getElementById("Encontrado").innerText ="Superheroe Encontrado";
 document.getElementById("texto").innerText ="Nombre: " + d.name;
 document.getElementById("texto2").innerText ="Conexiones: " + d.connections["group-affiliation"];
 document.getElementById("texto3").innerText = "Publicado por: " + d.biography.publisher;
-document.getElementById("texto4").innerText ="Ocupacion: " + d.work.occupation;
-document.getElementById("texto5").innerText = "Primera Aparicion: " + d.biography["first-appearance"];
+document.getElementById("texto4").innerText ="Ocupación: " + d.work.occupation;
+document.getElementById("texto5").innerText = "Primera Aparición: " + d.biography["first-appearance"];
 document.getElementById("texto6").innerText ="Altura: " + d.appearance.height[1];
 document.getElementById("texto7").innerText ="Peso: " + d.appearance.weight[1];
 document.getElementById("texto8").innerText ="Alianzas: " + d.biography.aliases;
 
-//Grafico JS
+
+
+//Grafico de Estadisticas
 
     var options = {
         title: {
-            text: "Estadisticas de Poder para: " + d.name
+            text: "Estadísticas de Poder para: " + d.name
             
         },
         data: [{
@@ -49,8 +54,7 @@ document.getElementById("texto8").innerText ="Alianzas: " + d.biography.aliases;
     
     
 
-console.log(d);       
-            
+
 })
     
 })
